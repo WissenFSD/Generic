e2f0c75940332ed4f4ab05a1b2a8a2924ddbd619
BRIGHT <BRIGHT@DESKTOP-ST51CUU>
2024-01-28 13:13:08 +0300

    Add project files.

---
+++ Generic(Kisitlar)/Program.cs
@@ -0,0 +1,141 @@
+﻿namespace Generic_Kisitlar_
+{
+
+    interface IGeneric
+    {
+
+    }
+    class Wissen : IGeneric
+    {
+        public Wissen(string a)
+        {
+
+        }
+    }
+    class SubWissen : Wissen
+    {
+        public SubWissen(string a) : base(a)
+        {
+
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            // Generic kavramında, generic olarak belirlenmiş metotlara gelebilecek olan parametrelere sınır getirebiliriz.
+
+
+            // Aşağıda getirebileceğimiz 5 adet farklı kısıt mekanizması incelenmiştir.
+
+
+
+            // Class kısıtı olan bir metoda struct parametre gönderilemez.
+            // Generic1<int>(10);
+
+
+            // Generic2 metodu struct kısıtı olduğu için int tipini sorunsuz parametre olarak aldı
+            //Generic2<int>(10);
+
+
+            // class kısıtı olan generic metoda Random sınıfını gönderelim
+            //Random bir class olduğu için class kısıtı olan metoda parametre olarak gönderildi.
+            //Generic1<Random>(new Random());
+
+
+            //Default ctor kısıtı
+            //Generic3<Wissen>(new Wissen());
+
+
+            // Kalıtım kısıtı
+            //Generic4<SubWissen>(new SubWissen(""));
+
+
+
+            // Interface kısıtı
+            Generic5<Wissen>(new Wissen(""));
+        }
+
+
+
+        // Generic kısıtlar, temel olarak generic olarak kullanılacak olan tüm tiplere kısıtlar koymaya denir
+
+        // M sadece class olabilir(Referans type olabilir)
+        static void Generic1<M>(M value) where M : class
+        {
+
+            Console.WriteLine(value);
+        }
+
+        // M sadece struct olabilir(Value type olabilir)
+        static void Generic2<M>(M value) where M : struct
+        {
+
+            Console.WriteLine(value);
+        }
+        // M yerine gelecek olan tiplerin default ctor'u olması lazım
+        // Her sınıfta eğer siz bir ctor yazmazsanız derleyici bir default ctor yazar
+        // bu örnekte wissen class'ını kullandık.
+        // Wissen class'ınına bir ctor yazmadığınızda bu metoda parametre olarak gönderilebilir.
+        // ancak bir parametreli ctor yazdığınızda bu metoda parametre olarak gönderilirse derleme zamanı hatası alacaktır
+        static void Generic3<M>(M value) where M : new()
+        {
+            Console.WriteLine(value);
+        }
+
+        // Bu metoda ya wissen sınıfı gelebilir, yada wissen sınıfından kalıtılan sınıflar gelebilir
+        // Bu örnek için wissen sınıfından kalıtılan SubWissen sınıfı kullanıldı
+        static void Generic4<M>(M value) where M : Wissen
+        {
+
+            Console.WriteLine(value);
+        }
+
+        // M yerine gelecek tüm değerler, kesinlikle IGeneric interfacesini implement etmek zorundadır.
+        static void Generic5<M>(M value) where M : IGeneric
+        {
+
+
+        }
+        // çoklu kısıt
+
+        // M yerine gelecek olan değer IGeneric interfacesini implement etmiş olacak ve default ctor'u olacak
+        static void Generic6<M>(M value) where M : IGeneric, new()
+        {
+
+
+        }
+
+
+
+
+        // Çoklu
+
+        // M hem wissen sınıfından katılılmalı hemde IGeneric interfacesini implement etmeli
+        static void Generic7<M>(M value)where M :Wissen, IGeneric
+        {
+
+
+        }
+
+        // M referans type olmalı ve IGeneric interfacesini implement etmiş olmalıdır.
+        static void Generic8<M>(M value)where M:class,IGeneric
+        {
+
+        }
+
+
+
+        // bu generic interfacelerden sonra konuşulacak
+        static void Generic9<M>(M value)where M:class,ICollection<M>
+        {
+
+
+
+        }
+    }
+}
---
+++ Generic/Program.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+
+namespace Generic
+{
+    public class Generic
+    {
+        public string MYProp { get; set; }
+        public Guid Guid { get; set; }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Generic konusu c# 3.0 ile birlikte hayatımıza giren bir konudur.
+            // Genericê  temel olarak şablon oluşturma  diyebiliriz.
+
+            //Genericler tasarladığımız interface,class,method,koleksiyon'ların yada bunların parametrelerinin belirli bir tip için değil
+            // şablon yapısına uyan tüm tipler için çalışmasını sağlayan bir teknolojidir.
+
+
+            // Generic'in faydaları
+
+            // Generic ile her tipte metot yapabildiğimiz için fazla kod yazmaya gerek kalmaz
+            // her tipi tek metotta karşılayabildiğimiz için ekonoik kod yazmış oluruz.
+
+
+
+            int veri = Method("veri");
+
+            // Tek parametreli ve denüş değeri olan generic method
+            int value = GenericMethod<int, string>("Value");
+            string stringValue = GenericMethod<string, int>(10);
+
+
+            // iki parametreli generic method
+
+            float floatValue = GenericMethodWithParameters<float, byte, char>(200, 'A');
+
+
+
+            // Kendi yazdığım Generic parametre alan metot sayesinde aşağıda görünebileceği üzere farklı tipteki parametreleri generic metot'uma gönderebildim.
+            ReturnAllProperty<Generic>(new Generic());
+            //ReturnAllProperty<Random>(new Random());
+            //ReturnAllProperty<ArrayList>(new ArrayList());
+            //ReturnAllProperty<Decimal>(10);
+            //ReturnAllProperty<Queue>(new Queue());
+
+
+        }
+        public static void ReturnAllProperty<M>(M value)
+        {
+            // bu bir reflection örneğidir.
+
+            var props = value.GetType().GetProperties();
+            foreach (var item in props)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        public static int Method(string value)
+        {
+            return 0;
+        }
+        public static K GenericMethod<K, M>(M value)
+        {
+            return default(K);
+        }
+        static K GenericMethodWithParameters<K, M, L>(M mvalue, L lvalue)
+        {
+            return default(K);
+        }
+
+        // Generic parametre alan ve geriye değer döndürmeyen metot
+        static void Method1<M>(M value)
+        {
+
+        }
+        // Generic parametre alan geriye int döndüren metot
+        static int Method2<M>(M value)
+        {
+            return 0;
+        }
+        // Generic alan ve generic döndüren metot(Parametre ile geri dönüş değeri aynı)
+        static M Method3<M>(M value)
+        {
+            return value;
+        }
+    }
+}
